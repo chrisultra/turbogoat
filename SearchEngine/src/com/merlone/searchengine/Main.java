47d9ce5d5cf46c6103eee6c67ff5820734e0d531
chrisultra <chrismerlone@yahoo.com>
2017-02-17 02:59:30 -0500

    Uploaded Main run file.

---
+++ SearchEngine/src/com/merlone/searchengine/Main.java
@@ -0,0 +1,12 @@
+package com.merlone.searchengine;
+
+public class Main {
+    
+    public static void main(String[] args){
+    
+        SearchEngine form = new SearchEngine();
+        form.setVisible(true);
+    
+    }
+    
+}
